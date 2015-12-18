04f49c6cf6f1040b7f99202d598c01c47f395cc8
Martin Swiderski <martin.swiderski@gmail.com>
2015-12-18 17:35:18 +0000

    wip

---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    checkCredits: function() {
+        return "not implemented";
+    },
+
+    checkMessageStatus: function() {
+        return "not implemented";
+    },
+
+    sendMessage: function() {
+        return "not implemented";
+    },
+
+    reports: function() {
+        return "not implemented";
+    },
+
+    createUser: function() {
+        return "not implemented";
+    },
+
+    updateCredits: function() {
+        return "not implemented";
+    },
+
+    importContactsCsv: function() {
+        return "not implemented";
+    },
+
+    deleteAllContacts: function() {
+        return "not implemented";
+    },
+
+    deleteAllGroups: function() {
+        return "not implemented";
+    },
+
+    emptyGroup: function() {
+        return "not implemented";
+    },
+
+    deleteGroup: function() {
+        return "not implemented";
+    },
+
+    getBgMessages: function() {
+        return "not implemented";
+    }
+
+};
---
+++ tests/specs/node-fastsms-spec.js
@@ -0,0 +1,19 @@
+var nodeFastSms = require('../../index');
+
+describe("Core tests", function () {
+    // implementation not ready yet...
+    it("Methods not implemented", function () {
+        expect(nodeFastSms.checkCredits()).toBe("not implemented");
+        expect(nodeFastSms.checkMessageStatus()).toBe("not implemented");
+        expect(nodeFastSms.sendMessage()).toBe("not implemented");
+        expect(nodeFastSms.reports()).toBe("not implemented");
+        expect(nodeFastSms.createUser()).toBe("not implemented");
+        expect(nodeFastSms.updateCredits()).toBe("not implemented");
+        expect(nodeFastSms.importContactsCsv()).toBe("not implemented");
+        expect(nodeFastSms.deleteAllContacts()).toBe("not implemented");
+        expect(nodeFastSms.deleteAllGroups()).toBe("not implemented");
+        expect(nodeFastSms.emptyGroup()).toBe("not implemented");
+        expect(nodeFastSms.deleteGroup()).toBe("not implemented");
+        expect(nodeFastSms.getBgMessages()).toBe("not implemented");
+    });
+});
