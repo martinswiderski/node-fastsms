e450f3c12c3858fc59a789bc1e7378ea22dd46fd
Martin Swiderski <martin.swiderski@gmail.com>
2015-12-21 21:33:28 +0000

    updated

--- tests/specs/url-spec.js
+++ tests/specs/url-spec.js
@@ -2,10 +2,10 @@ var url = require(__dirname + "/../../src/url");
 
 describe("Can build URL", function () {
 
-    var correct = url.build('http', '127.0.0.1', '/api/', { session: 'ON', hello: 'World'});
+    var correct = url.build('http', '127.0.0.1', '/api/', { session: 'ON', hello: 'World&'});
 
-    it("having correct attributes passed", function () {
-        expect(correct).toBe('http://127.0.0.1/api/?session=ON&hello=World');
+    it("as escaped string having correct attributes passed", function () {
+        expect(correct).toBe('http://127.0.0.1/api/?session=ON&hello=World%26');
     });
 });
 
@@ -13,7 +13,7 @@ describe("Throws errors", function () {
 
     var boo = url.build('http', '127.0.0.1', '/api/', false);
 
-    it("for incorrect input", function () {
+    it("for incorrect input FALSE is returned", function () {
         expect(boo).toBe(false);
     });
 });
