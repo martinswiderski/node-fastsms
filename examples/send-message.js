fed60978ad18d0435c57ac1cd64b2ae76c7e0486
Martin Swiderski <martin.swiderski@gmail.com>
2015-12-28 18:19:17 +0000

    wip

--- examples/send-message.js
+++ examples/send-message.js
@@ -4,7 +4,7 @@
  * mobile in format 447777112233
  */
 var fastSms = require(__dirname + "/../src/fast-sms").setConfig(require(__dirname + "/../src/configuration")),
-    sentId  = fastSms.sendOne(process.argv[2], "That's one small step for a man, one giant leap for mankind", 'Boo');
+    sentId  = fastSms.sendOne(process.argv[2], "That's one small step for a man, one giant leap for mankind", 'TestMessage');
 
 if (!isNaN(sentId) && sentId > 0) {
     console.log('Message sent. ID: ' + sentId);
