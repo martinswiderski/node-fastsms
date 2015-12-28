ba1472377c3bc5d727c309233b99611ee92945e6
Martin Swiderski <martin.swiderski@gmail.com>
2015-12-28 18:17:41 +0000

    examples

---
+++ examples/send-message.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+/**
+ * To run invoke ./send-message.js YOURMOBILE
+ * mobile in format 447777112233
+ */
+var fastSms = require(__dirname + "/../src/fast-sms").setConfig(require(__dirname + "/../src/configuration")),
+    sentId  = fastSms.sendOne(process.argv[2], "That's one small step for a man, one giant leap for mankind", 'Boo');
+
+if (!isNaN(sentId) && sentId > 0) {
+    console.log('Message sent. ID: ' + sentId);
+} else {
+    console.log("Message not sent. Check environment variables for FAST_SMS_API_TOKEN\nUse: \nexport FAST_SMS_API_TOKEN=''\nto set the value");
+}
+
+
--- examples/send.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FAST_SMS_API_TOKEN=''
-echo $FAST_SMS_API_TOKEN
-node send-message.js
