b04d5ca663a3eb0c53ee6942dfd2f74760476b12
Martin Swiderski <martin.swiderski@gmail.com>
2015-12-29 15:12:56 +0000

    example updated

--- examples/send-message.js
+++ examples/send-message.js
@@ -3,7 +3,7 @@
  * To run invoke ./send-message.js YOURMOBILE
  * mobile in format 447777112233
  */
-var fastSms = require(__dirname + "/../src/fast-sms").setConfig(require(__dirname + "/../src/configuration")),
+var fastSms = require(__dirname + "/../index").setConfig(require(__dirname + "/../src/configuration")),
     sentId  = fastSms.sendOne(process.argv[2], "That's one small step for a man, one giant leap for mankind.\n\nPS. SMS module is fully operational.", 'Martin');
 
 if (!isNaN(sentId) && sentId > 0) {
