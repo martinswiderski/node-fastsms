b777a1d60da6c849ac36ca5d231deda9f15b798f
Martin Swiderski <martin.swiderski@gmail.com>
2016-02-09 22:20:24 +0000

    operations code  UT

--- src/op-code.js
+++ src/op-code.js
@@ -1,94 +1,102 @@
 var opCode = function opCode() {
 
+    /**
+     * These are HTTP Codes
+     * in @key {int}: @value {string}
+     */
     this.httpCode = {
-        100: 'Continue',
-        101: 'Switching Protocols',
-        102: 'Processing (WebDAV; RFC 2518)',
-        200: 'OK',
-        201: 'Created',
-        202: 'Accepted',
-        203: 'Non-Authoritative Information (since HTTP/1.1)',
-        204: 'No Content',
-        205: 'Reset Content',
-        206: 'Partial Content (RFC 7233)',
-        207: 'Multi-Status (WebDAV; RFC 4918)',
-        208: 'Already Reported (WebDAV; RFC 5842)',
-        226: 'IM Used (RFC 3229)',
-        300: 'Multiple Choices',
-        301: 'Moved Permanently',
-        302: 'Found',
-        303: 'See Other (since HTTP/1.1)',
-        304: 'Not Modified (RFC 7232)',
-        305: 'Use Proxy (since HTTP/1.1)',
-        306: 'Switch Proxy',
-        307: 'Temporary Redirect (since HTTP/1.1)',
-        308: 'Permanent Redirect (RFC 7538)',
-        404: 'error on German Wikipedia',
-        400: 'Bad Request',
-        401: 'Unauthorized (RFC 7235)',
-        402: 'Payment Required',
-        403: 'Forbidden',
-        404: 'Not Found',
-        405: 'Method Not Allowed',
-        406: 'Not Acceptable',
-        407: 'Proxy Authentication Required (RFC 7235)',
-        408: 'Request Timeout',
-        409: 'Conflict',
-        410: 'Gone',
-        411: 'Length Required',
-        412: 'Precondition Failed (RFC 7232)',
-        413: 'Payload Too Large (RFC 7231)',
-        414: 'URI Too Long (RFC 7231)',
-        415: 'Unsupported Media Type',
-        416: 'Range Not Satisfiable (RFC 7233)',
-        417: 'Expectation Failed',
-        418: 'I'm a teapot (RFC 2324)',
-    419: 'Authentication Timeout (not in RFC 2616)',
-        421: 'Misdirected Request (RFC 7540)',
-        422: 'Unprocessable Entity (WebDAV; RFC 4918)',
-        423: 'Locked (WebDAV; RFC 4918)',
-        424: 'Failed Dependency (WebDAV; RFC 4918)',
-        426: 'Upgrade Required',
-        428: 'Precondition Required (RFC 6585)',
-        429: 'Too Many Requests (RFC 6585)',
-        431: 'Request Header Fields Too Large (RFC 6585)',
-        451: 'Unavailable For Legal Reasons (Internet draft)',
-        500: 'Internal Server Error',
-        501: 'Not Implemented',
-        502: 'Bad Gateway',
-        503: 'Service Unavailable',
-        504: 'Gateway Timeout',
-        505: 'HTTP Version Not Supported',
-        506: 'Variant Also Negotiates (RFC 2295)',
-        507: 'Insufficient Storage (WebDAV; RFC 4918)',
-        508: 'Loop Detected (WebDAV; RFC 5842)',
-        510: 'Not Extended (RFC 2774)',
-        511: 'Network Authentication Required (RFC 6585)',
-        420: 'Method Failure (Spring Framework)',
-        420: 'Enhance Your Calm (Twitter)',
-        450: 'Blocked by Windows Parental Controls (Microsoft)',
-        498: 'Invalid Token (Esri)',
-        499: 'Token Required (Esri)',
-        509: 'Bandwidth Limit Exceeded (Apache Web Server/cPanel)',
-        440: 'Login Timeout',
-        449: 'Retry With',
-        451: 'Redirect',
-        444: 'No Response',
-        495: 'SSL Certificate Error',
-        496: 'SSL Certificate Required',
-        497: 'HTTP Request Sent to HTTPS Port',
-        499: 'Client Closed Request',
-        520: 'Unknown Error',
-        521: 'Web Server Is Down',
-        522: 'Connection Timed Out',
-        523: 'Origin Is Unreachable',
-        524: 'A Timeout Occurred',
-        525: 'SSL Handshake Failed',
-        526: 'Invalid SSL Certificate'
+        '100': 'Continue',
+        '101': 'Switching Protocols',
+        '102': 'Processing (WebDAV; RFC 2518)',
+        '200': 'OK',
+        '201': 'Created',
+        '202': 'Accepted',
+        '203': 'Non-Authoritative Information (since HTTP/1.1)',
+        '204': 'No Content',
+        '205': 'Reset Content',
+        '206': 'Partial Content (RFC 7233)',
+        '207': 'Multi-Status (WebDAV; RFC 4918)',
+        '208': 'Already Reported (WebDAV; RFC 5842)',
+        '226': 'IM Used (RFC 3229)',
+        '300': 'Multiple Choices',
+        '301': 'Moved Permanently',
+        '302': 'Found',
+        '303': 'See Other (since HTTP/1.1)',
+        '304': 'Not Modified (RFC 7232)',
+        '305': 'Use Proxy (since HTTP/1.1)',
+        '306': 'Switch Proxy',
+        '307': 'Temporary Redirect (since HTTP/1.1)',
+        '308': 'Permanent Redirect (RFC 7538)',
+        '400': 'Bad Request',
+        '401': 'Unauthorized (RFC 7235)',
+        '402': 'Payment Required',
+        '403': 'Forbidden',
+        '404': 'Not Found',
+        '405': 'Method Not Allowed',
+        '406': 'Not Acceptable',
+        '407': 'Proxy Authentication Required (RFC 7235)',
+        '408': 'Request Timeout',
+        '409': 'Conflict',
+        '410': 'Gone',
+        '411': 'Length Required',
+        '412': 'Precondition Failed (RFC 7232)',
+        '413': 'Payload Too Large (RFC 7231)',
+        '414': 'URI Too Long (RFC 7231)',
+        '415': 'Unsupported Media Type',
+        '416': 'Range Not Satisfiable (RFC 7233)',
+        '417': 'Expectation Failed',
+        '418': 'I\'m a teapot (RFC 2324)',
+        '419': 'Authentication Timeout (not in RFC 2616)',
+        '421': 'Misdirected Request (RFC 7540)',
+        '422': 'Unprocessable Entity (WebDAV; RFC 4918)',
+        '423': 'Locked (WebDAV; RFC 4918)',
+        '424': 'Failed Dependency (WebDAV; RFC 4918)',
+        '426': 'Upgrade Required',
+        '428': 'Precondition Required (RFC 6585)',
+        '429': 'Too Many Requests (RFC 6585)',
+        '431': 'Request Header Fields Too Large (RFC 6585)',
+        '451': 'Unavailable For Legal Reasons (Internet draft)',
+        '500': 'Internal Server Error',
+        '501': 'Not Implemented',
+        '502': 'Bad Gateway',
+        '503': 'Service Unavailable',
+        '504': 'Gateway Timeout',
+        '505': 'HTTP Version Not Supported',
+        '506': 'Variant Also Negotiates (RFC 2295)',
+        '507': 'Insufficient Storage (WebDAV; RFC 4918)',
+        '508': 'Loop Detected (WebDAV; RFC 5842)',
+        '510': 'Not Extended (RFC 2774)',
+        '511': 'Network Authentication Required (RFC 6585)',
+        '420': 'Method Failure (Spring Framework)',
+        '420': 'Enhance Your Calm (Twitter)',
+        '450': 'Blocked by Windows Parental Controls (Microsoft)',
+        '498': 'Invalid Token (Esri)',
+        '499': 'Token Required (Esri)',
+        '509': 'Bandwidth Limit Exceeded (Apache Web Server/cPanel)',
+        '440': 'Login Timeout',
+        '449': 'Retry With',
+        '451': 'Redirect',
+        '444': 'No Response',
+        '495': 'SSL Certificate Error',
+        '496': 'SSL Certificate Required',
+        '497': 'HTTP Request Sent to HTTPS Port',
+        '499': 'Client Closed Request',
+        '520': 'Unknown Error',
+        '521': 'Web Server Is Down',
+        '522': 'Connection Timed Out',
+        '523': 'Origin Is Unreachable',
+        '524': 'A Timeout Occurred',
+        '525': 'SSL Handshake Failed',
+        '526': 'Invalid SSL Certificate',
+        '0': 'Not defined'
     },
 
-
-    this.errorCode = {
+    /**
+     * These are specific errors
+     * as per FastSMS definition
+     * in @key {int}: @value {string}
+     */
+    this.apiError = {
         '-100': 'Not Enough Credits',
         '-101': 'Invalid CreditID',
         '-200': 'Invalid Contact',
@@ -129,11 +137,44 @@ var opCode = function opCode() {
         '-601': 'Unknown Report Type',
         '-701': 'No UserID Specified',
         '-702': 'Invalid Amount Specified',
-        '-703': 'Invalid Currency Requested'
+        '-703': 'Invalid Currency Requested',
+        '-1': 'Not defined'
     },
 
-    this.resolve = function (code) {
-        return (this.error[code]) ? this.error[code] : false;
+    /**
+     * Resolves codes and returns envelope
+     * @param {int} httpCode HTTP code
+     * @param {int} apiCode  API code
+     * @return {object}
+     */
+    this.resolve = function (httpCode, apiCode) {
+
+        apiCode  = (this.integerOrFalse(apiCode) === false) ? -1 : parseInt(apiCode);
+        httpCode = (this.integerOrFalse(httpCode) === false) ? 0 : parseInt(httpCode);
+
+        var apiDetails  = (this.apiError[apiCode]) ? 'API Error: ' + this.apiError[apiCode] : false,
+            httpDetails = (this.httpCode[httpCode]) ? 'HTTP Status: ' + this.httpCode[httpCode] : false,
+            response    = {
+                api: {
+                    code: (false === apiDetails) ? false : apiCode,
+                    status: apiDetails
+                },
+                http: {
+                    code: httpCode,
+                    status: httpDetails
+                }
+            };
+        return response;
+    },
+
+    /**
+     * Checks string and if it's possible to cast type to INT then it returns integer
+     * otherwise it returns false
+     * @param string str String to examine
+     * @return {bool|int}
+     */
+    this.integerOrFalse = function (str) {
+        return ('' + parseInt(str) === 'NaN') ? false : parseInt(str);
     };
 };
 
---
+++ tests/specs/op-code-spec.js
@@ -0,0 +1,43 @@
+var opCode = require(__dirname + "/../../src/op-code");
+
+describe("Taking HTTP response code and possible API Err Code", function () {
+
+    it("checks if cast-able to integer", function () {
+        expect(opCode.integerOrFalse('')).toBe(false);
+        expect(opCode.integerOrFalse('ABC')).toBe(false);
+        expect(opCode.integerOrFalse(false)).toBe(false);
+        expect(opCode.integerOrFalse(true)).toBe(false);
+        expect(opCode.integerOrFalse(null)).toBe(false);
+
+        expect(opCode.integerOrFalse(1234.45)).toBe(1234);
+        expect(opCode.integerOrFalse('1234.45')).toBe(1234);
+        expect(opCode.integerOrFalse('234')).toBe(234);
+        expect(opCode.integerOrFalse(123456)).toBe(123456);
+    });
+
+    var invalidUser = opCode.resolve("200", "-509");
+    it("invalid credentials are picked up", function () {
+        expect(invalidUser.api.code).toBe(-509);
+        expect(invalidUser.api.status).toBe('API Error: Unknown/Invalid User');
+        expect(invalidUser.http.code).toBe(200);
+        expect(invalidUser.http.status).toBe('HTTP Status: OK');
+    });
+
+    var noHttpCodeNoApiError = opCode.resolve(null, null);
+    it("picks up no valid HTTP code and no Err API responses (ones does not make much sense)", function () {
+        expect(noHttpCodeNoApiError.api.code).toBe(-1);
+        expect(noHttpCodeNoApiError.api.status).toBe('API Error: Not defined');
+        expect(noHttpCodeNoApiError.http.code).toBe(0);
+        expect(noHttpCodeNoApiError.http.status).toBe('HTTP Status: Not defined');
+    });
+
+    var goodCall = opCode.resolve("200", "");
+    it("good call has no errors", function () {
+        expect(goodCall.api.code).toBe(-1);
+        expect(goodCall.api.status).toBe('API Error: Not defined');
+        expect(goodCall.http.code).toBe(200);
+        expect(goodCall.http.status).toBe('HTTP Status: OK');
+    });
+    
+});
+
