78cb32a75eb8437e212c945d17250bfe7015fef2
Martin Swiderski <martin.swiderski@gmail.com>
2016-02-14 00:50:43 +0000

    wip

--- tests/specs/response-spec.js
+++ tests/specs/response-spec.js
@@ -16,11 +16,17 @@ describe("Reads package version", function () {
 });
 
 describe("Generate response for HTTP transaction in JSON envelope", function () {
-
     var message = 'This is my Message ',
-        resp = response.render('-509', 200, md5(message), message.length, 234, false);
-    console.log(resp);
-    it("Blank object", function () {
-        //expect(response.resolveModuleVersion()+'' === '0.1.32').toBe(true);
+        resp    = response.render('-509', 200, md5(message), message.length, 234, false);
+
+    //console.log(resp);
+    it("Even blank and incomplete object contains configuration & version id", function () {
+        expect(resp.version === '0.1.32').toBe(true);
+        expect(resp.config.hostname === 'A').toBe(true);
+        expect(resp.config.protocol === 'B').toBe(true);
+        expect(resp.config.path === 'C').toBe(true);
+        expect(resp.config.token === 'Check your FAST_SMS_API_TOKEN').toBe(true);
+        expect(resp.config.instanceId === 'E').toBe(true);
     });
 });
+
