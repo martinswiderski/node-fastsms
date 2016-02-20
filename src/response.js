7e06cc9c818c95c180e12b6e0136c09935909ea1
Martin Swiderski <martin.swiderski@gmail.com>
2016-02-20 21:11:36 +0000

    response

--- src/response.js
+++ src/response.js
@@ -1,6 +1,11 @@
 var response,
+    unixTime      = require('unix-time'),
+    uuid          = require('uuid'),
+    fs            = require('fs'),
+    md5           = require('md5'),
     configuration = require('./configuration'),
-    errorCode     = require('./error-code');
+    opCode        = require('./op-code'),
+    actions       = require('./actions');
 
 response = function response() {
 
@@ -9,42 +14,132 @@ response = function response() {
      * @var {object}
      */
     this.envelope = {
-        isError: false,
-        responseCode: 0,
-        creditsLeft: 0,
-        messageUID: '',
-        moduleVersion: '',
-        executionDetails: {
-            host: configuration.host,
-            protocol: configuration.protocol,
+        version: false,
+        action: '',
+        error: {
+            status: false,
+            details: []
+        },
+        message: {
+            id: 0,
+            uuid: '',
+            content: {
+                source: '',
+                target: '',
+                md5: '',
+                length: 0
+            },
             timestamp: '',
-            timestampUnix: 0,
-            msExec: 0
+            unix: 0,
+            exec: 0
+        },
+        api: {
+            code: false,
+            status: ''
+        },
+        http: {
+            code: false,
+            status: ''
+        },
+        credits: false,
+        config: {
+            hostname: configuration.hostname,
+            protocol: configuration.protocol,
+            path: configuration.path,
+            token: md5(configuration.token),
+            instanceId: configuration.instanceId
         },
         persistence: {
+
         }
     },
 
-    this.resolveModuleVersion = function() {
-        var package = JSON.parse(__dirname + '/../package.json');
-        return (package['version']) ? package['version'] : 0;
+    /**
+     * Turns subject into INT or FALSE
+     * @param string str Subject string to turn to INT or FALSE
+     * @return {int|bool}
+     */
+    this.integerOrFalse = function (str) {
+        return ('' + parseInt(str) === 'NaN') ? false : parseInt(str);
     },
 
     /**
-     * Full response returned
-     * @param int httpCode        HTTP Response code, if non-200 it's an error
-     * @param int messageReturned Message returned (if negative it's error)
-     * @returns {object}
+     * Resolves response details
+     * @param string    opType        Operation type, i.e. 'Send'
+     * @param int|mixed apiReturned   API Code (negative=> error, otherwise=> messageId)
+     * @param int|mixed httpCode      HTTP Code from HTTP REST Client
+     * @param string    messageMd5    MD5 of the message sent
+     * @param int|mixed messageLength Length of the message sent
+     * @param int|mixed execTime      Execution in ms
+     * @param bool     checkCredits   flag true|false checking credits
+     * @param function checkCreditsFn If above is true called to fetch credits-left value
+     * @returns {}
      */
-    this.returnResponse = function(httpCode, messageReturned) {
-        httpCode        = parseInt(httpCode);
-        messageReturned = parseInt(messageReturned);
-        var out         = JSON.parse(JSON.stringify(this.envelope)),
-            errCode     = errorCode.resolve(messageReturned);
+    this.render = function (opType, apiReturned, httpCode, messageMd5, messageLength, execTime, checkCredits, checkCreditsFn) {
 
-        out.responseCode = errCode;
+        var date        = new Date(),
+            response    = JSON.parse(JSON.stringify(this.envelope));
 
-        return out;
+        response.version = this.resolveModuleVersion();
+
+        if (actions.isValid(opType) !== true) {
+            throw new Error('Invalid action: ' + opType);
+        }
+
+        response.action = opType;
+
+        checkCredits = (false === checkCredits || true === checkCredits) ? checkCredits : false;
+
+        apiReturned = this.integerOrFalse(apiReturned),
+        httpCode    = this.integerOrFalse(httpCode);
+        apiReturned = (false === apiReturned) ? -1 : apiReturned;
+        httpCode    = (false === httpCode) ? 0 : httpCode;
+
+        response.message.content.md5    = messageMd5;                          // 32 chars
+        response.message.content.length = messageLength;
+        response.message.timestamp      = date;
+        response.message.unix           = unixTime(date);                // unit-timestamp
+        response.message.exec           = this.integerOrFalse(execTime);       // number
+        response.message.uuid           = uuid.v1();                           // 36 chars
+        response.message.id             = (apiReturned < 0) ? 0 : apiReturned; // number
+        response.credits                = (checkCredits === true && (typeof checkCreditsFn) === 'function') ? checkCreditsFn() : false;
+
+        var apiDetails  = opCode.resolve(apiReturned, 'api'),
+            httpDetails = opCode.resolve(httpCode, 'http');
+
+        response.http.code   = httpCode;
+        response.http.status = httpDetails;
+        response.api.code    = (apiReturned>0) ? false : apiReturned;
+        response.api.status  = apiDetails;
+
+        if (parseInt(response.http.code) > 299 || parseInt(response.http.code) < 200) {
+            response.error.details.push({
+                type: 'HTTP',
+                code: response.http.code,
+                status : response.http.status
+            });
+        }
+
+        if (parseInt(response.api.code) < 0) {
+            response.error.details.push({
+                type: 'API',
+                code: response.api.code,
+                status : response.api.status
+            });
+        }
+
+        if (response.error.details.length > 0) {
+            response.error.status = true;
+        }
+
+        return response;
+    },
+
+    this.resolveModuleVersion = function() {
+        var pckg = JSON.parse(
+            fs.readFileSync(__dirname + '/../package.json').toString()
+        );
+        return (!pckg['version']) ? 0 : pckg['version'];
     };
 };
 
---
+++ tests/specs/response-spec.js
@@ -0,0 +1,123 @@
+process.env['FAST_SMS_API_UNIT_TEST']       = '1';
+process.env['FAST_SMS_API_HOSTNAME']        = 'A';
+process.env['FAST_SMS_API_PROTOCOL']        = 'B';
+process.env['FAST_SMS_API_PATH']            = 'C';
+process.env['FAST_SMS_API_TOKEN']           = 'D';
+process.env['FAST_SMS_API_CLIENT_INSTANCE'] = 'E';
+
+var md5      = require('md5'),
+    response = require(__dirname + "/../../src/response");
+
+var message   = 'This is my Message ',
+    exec      = 234,
+    chkCredits = true,
+    respBlank = response.render('Send', '-509', 404, md5(message), message.length, exec, chkCredits, mockGetCreditsFunction),
+    respValid = {};
+
+function mockGetCreditsFunction() {
+    return 1234567890;
+}
+// --- Assertions
+describe("Reads package version", function () {
+    it("from package.json file", function () {
+        expect(response.resolveModuleVersion()+'' === '0.1.32').toBe(true);
+    });
+});
+
+describe("Generates response for failed HTTP transaction in JSON envelope", function () {
+    //console.log(JSON.stringify(respBlank, null, 4));
+
+    it("Even blank and incomplete object contains configuration & version id", function () {
+        expect(respBlank.version === '0.1.32').toBe(true);
+        expect(respBlank.action === 'Send').toBe(true);
+        expect(respBlank.config.hostname === 'A').toBe(true);
+        expect(respBlank.config.protocol === 'B').toBe(true);
+        expect(respBlank.config.path === 'C').toBe(true);
+        expect(respBlank.config.token === md5('D')).toBe(true); // 'D' is the value set by mock
+        expect(respBlank.config.instanceId === 'E').toBe(true);
+    });
+
+    it("When flag is passed and external function it is called to calculate credits", function () {
+        expect(chkCredits).toBe(true);
+        expect(respBlank.credits === mockGetCreditsFunction()).toBe(true);
+    });
+
+    it("API and HTTP information populated if error codes are provided", function () {
+        expect(respBlank.error.status === true).toBe(true);
+        expect((typeof respBlank.http.code) === 'number').toBe(true);
+        expect(respBlank.http.code === 404).toBe(true);
+        expect(respBlank.http.status === 'Not Found').toBe(true);
+        expect((typeof respBlank.api.code) === 'number').toBe(true);
+        expect(respBlank.api.code === -509).toBe(true);
+        expect(respBlank.api.status === 'Unknown/Invalid User').toBe(true);
+        expect(respBlank.error.details.length > 0).toBe(true);
+        expect((typeof respBlank.error.details[0]) === 'object').toBe(true);
+        expect(respBlank.error.details[0].type === 'HTTP').toBe(true);
+        expect(respBlank.error.details[0].code === 404).toBe(true);
+        expect(respBlank.error.details[0].status === 'Not Found').toBe(true);
+        expect((typeof respBlank.error.details[1]) === 'object').toBe(true);
+        expect(respBlank.error.details[1].type === 'API').toBe(true);
+        expect(respBlank.error.details[1].code === -509).toBe(true);
+        expect(respBlank.error.details[1].status === 'Unknown/Invalid User').toBe(true);
+    });
+
+    it("Message length and MD5 are also set", function () {
+        expect(respBlank.version === '0.1.32').toBe(true);
+        expect(respBlank.message.content.md5 === md5(message)).toBe(true);
+        expect(respBlank.message.content.length === message.length).toBe(true);
+    });
+
+    it("Message is always given unique UID and numeric ID or zero is allocated", function () {
+        expect(respBlank.message.uuid.length === 36).toBe(true);
+        expect((typeof respBlank.message.id) === 'number').toBe(true);
+        expect(respBlank.message.id === 0).toBe(true);
+    });
+});
+
+
+respValid = response.render('Send', 234567, 200, md5(message), message.length, exec, chkCredits, mockGetCreditsFunction);
+
+describe("Generates response for a valid HTTP transaction in JSON envelope", function () {
+    //console.log(JSON.stringify(respValid, null, 4));
+
+    it("Message is always given unique UID and numeric ID or zero is allocated", function () {
+        expect(respBlank.action === 'Send').toBe(true);
+        expect(respValid.message.uuid.length === 36).toBe(true);
+        expect((typeof respValid.message.id) === 'number').toBe(true);
+        expect(respValid.message.id === 234567).toBe(true);
+    });
+
+    it("HTTP Status is 200, API error is empty & error status is FALSE", function () {
+        expect((typeof respValid.api.code) === 'boolean').toBe(true);
+        expect(respValid.api.code === false).toBe(true);
+        expect(respValid.api.status === false).toBe(true);
+        expect(respValid.http.code === 200).toBe(true);
+        expect(respValid.http.status === 'OK').toBe(true);
+        expect(respValid.error.details.length === 0).toBe(true);
+    });
+});
+
+
+describe("Throws error", function () {
+    it("on attempt to use invalid action", function () {
+        try {
+            var respWithBSAction = response.render(
+                'WatchTV',
+                234567,
+                200,
+                md5(message),
+                message.length,
+                exec,
+                chkCredits,
+                mockGetCreditsFunction
+            );
+
+            expect('Error was expected').toBe('and not raised'); // this should never be fired
+
+        } catch (errAct) {
+            expect(typeof errAct).toBe('object');
+            expect(errAct.message).toBe('Invalid action: WatchTV');
+        }
+    });
+});
+
